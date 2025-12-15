1c3efa59262631fbb408921e1ddc70b6e6b382e9
Arman <armani.kabidolda.18.12.03@gmail.com>
2025-12-15 17:32:00 +0500

    upd db.js

--- db.js
+++ db.js
@@ -2,16 +2,25 @@ import pkg from "pg";
 import dotenv from "dotenv";
 
 dotenv.config();
-
 const { Pool } = pkg;
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl:
-    process.env.NODE_ENV === "production"
-      ? { rejectUnauthorized: false }
-      : { rejectUnauthorized: false },
-});
+let pool;
+
+if (process.env.DATABASE_URL) {
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
+  });
+} else {
+  pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASS,
+    port: 5432,
+    ssl: false,
+  });
+}
 
 pool.on("connect", () => {
   console.log("Connected to PostgreSQL");
